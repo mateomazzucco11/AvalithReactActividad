d29713f124554f355390ee47c23f7e99b2e09de5
Mateo <mateomazzucco4@gmail.com>
2021-08-11 10:49:44 -0300

    adding products to productoslist

--- src/data/ProductosList.js
+++ src/data/ProductosList.js
@@ -2,5 +2,9 @@
 export const ProductosList = [
     { name: 'Pepsi', price: 200, description: 'Pepsi, también conocida como Pepsi-Cola, es una gaseosa de cola', stock: true },
     { name: 'Galletitas', price: 120, description: 'Galletitas surtidas', stock: false },
+    { name: 'Coca Cola', price: 70, description: 'Galletitas surtidas', stock: true },
+    { name: 'Sprite', price: 60, description: 'Galletitas surtidas', stock: true },
+    { name: 'Hamburguesas', price: 320, description: 'Galletitas surtidas', stock: true },
+    { name: 'Panchos', price: 100, description: 'Galletitas surtidas', stock: false },
 ]
 
